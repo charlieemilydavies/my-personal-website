678a66b838eeb226524fe07f68a61148515b2b2b
Charlie Davies <charlieemilydavies@gmail.com>
2024-06-03 18:02:51 -0700

    update css and html to new style

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
-const h2 = document.createElement("h2");
-h2.textContent = "This content added by JavaScript";
-
-document.querySelector("body").appendChild(h2);
+document.getElementById("moreButton").addEventListener("click", function () {
+  const extraText = document.getElementById("extraText");
+  if (extraText.classList.contains("hidden")) {
+    extraText.classList.remove("hidden");
+    this.textContent = "Show less";
+  } else {
+    extraText.classList.add("hidden");
+    this.textContent = "Find out more about me";
+  }
+});
